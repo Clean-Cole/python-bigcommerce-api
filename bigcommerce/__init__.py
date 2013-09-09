30224d8881c034c947cbfe6d3d7492d19097914c
Cole <cole@imakethe.com>
2013-09-08 20:14:26 -0500

    fix __init__

--- bigcommerce/__init__.py
+++ bigcommerce/__init__.py
@@ -1,7 +1,6 @@
 from .api import BigCommerce
-from .api import BigCommerceResponseError
+from .api import BigCommerceError
 #from .api import __version__
 
 __title__ = 'bigcommerce'
 __author__ = 'Coleman Stevenson'
-
